decb7b52ccccf18c60e2bd247174eaed3a532a2d
Luiz Filho <luizengsoft@gmail.com>
2025-11-08 21:49:42 -0400

    fix flat list

--- app/index.tsx
+++ app/index.tsx
@@ -2,20 +2,71 @@ import { IconButton } from "@/components/ui/icon-button";
 import { PokemonCard } from "@/components/ui/pokemon-card";
 import { SearchBar } from "@/components/ui/search-bar";
 import { TextColors } from "@/constants/theme";
-import { FireType, GrassType, PoisonType, WaterType } from "@/model/pokemon_type";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  FireType,
+  GrassType,
+  PoisonType,
+  WaterType,
+} from "@/model/pokemon_type";
+import {
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    number: "#001",
+    types: [new GrassType(), new PoisonType()],
+    image: require("@/assets/images/bulbasaur.png"),
+  },
+  {
+    name: "Charmander",
+    number: "#002",
+    types: [new FireType(), new PoisonType()],
+    image: require("@/assets/images/bulbasaur.png"),
+  },
+  {
+    name: "Squirtle",
+    number: "#003",
+    types: [new WaterType(), new PoisonType()],
+    image: require("@/assets/images/bulbasaur.png"),
+  },
+  {
+    name: "Bulbasaur",
+    number: "#004",
+    types: [new GrassType(), new PoisonType()],
+    image: require("@/assets/images/bulbasaur.png"),
+  },
+  {
+    name: "Charmander",
+    number: "#005",
+    types: [new FireType(), new PoisonType()],
+    image: require("@/assets/images/bulbasaur.png"),
+  },
+  {
+    name: "Squirtle",
+    number: "#006",
+    types: [new WaterType(), new PoisonType()],
+    image: require("@/assets/images/bulbasaur.png"),
+  },
+];
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <View style={{ flexDirection: "column" }}>
-        <ImageBackground
-          source={require("@/assets/images/gradient_pokeball.png")}
-          style={{ height: 200 }}
-        >
+    <SafeAreaView style={styles.safeArea} edges={["bottom"]}>
+      <ImageBackground
+        source={require("@/assets/images/gradient_pokeball.png")}
+        style={{paddingTop: 32}}
+      >
+        <View style={styles.header}>
           <TopBarActions />
-          <Text style={styles.header}>Pokédex</Text>
+          <Text style={styles.headerText}>Pokédex</Text>
           <Text style={styles.description}>
             Search for Pokémon by name or using the National Pokédex number.
           </Text>
@@ -24,32 +75,15 @@ export default function HomeScreen() {
             placeholder="What Pokémon are you looking for?"
             style={styles.searchBar}
           />
-          <PokemonCard
-            pokemon={{
-              name: "Bulbasaur",
-              number: "#001",
-              types: [new GrassType, new PoisonType],
-              image: require("@/assets/images/bulbasaur.png"),
-            }}
-          />
-          <PokemonCard
-            pokemon={{
-              name: "Charmander",
-              number: "#002",
-              types: [new FireType(), new PoisonType()],
-              image: require("@/assets/images/bulbasaur.png"),
-            }}
-          />
-          <PokemonCard
-            pokemon={{
-              name: "Squirtle",
-              number: "#003",
-              types: [new WaterType, new PoisonType],
-              image: require("@/assets/images/bulbasaur.png"),
-            }}
-          />
-        </ImageBackground>
-      </View>
+        </View>
+      </ImageBackground>
+      <FlatList
+        style={styles.list}
+        data={pokemonList}
+        renderItem={({ item }) => <PokemonCard pokemon={item} />}
+        keyExtractor={(item) => item.number}
+        showsVerticalScrollIndicator={false}
+      />
     </SafeAreaView>
   );
 }
@@ -74,13 +108,6 @@ function TopBarActions() {
 }
 
 const styles = StyleSheet.create({
-  header: {
-    fontSize: 32,
-    fontWeight: "bold",
-    paddingHorizontal: 32,
-    paddingTop: 35,
-    color: TextColors.black,
-  },
   description: {
     fontSize: 16,
     fontWeight: "400",
@@ -98,5 +125,23 @@ const styles = StyleSheet.create({
     marginTop: 24,
     marginBottom: 20,
     marginHorizontal: 32,
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  header: {
+    flexDirection: "column",
+  },
+  headerText: {
+    fontSize: 32,
+    fontWeight: "bold",
+    paddingHorizontal: 32,
+    paddingTop: 35,
+    color: TextColors.black,
+  },
+  list: {
+    flex: 3,
+    marginBottom: 16,
   },
 });
